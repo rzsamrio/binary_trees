63537c7ba7aac0ebf6669b6beae77cb5ffe3e3fb
rzsamrio <samuelekop144@gmail.com>
2024-02-28 22:22:43 +0100

    Test req

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	node = malloc(sizeof(binary_tree_t));
 
 	if (node == NULL)
-		return NULL
+		return (NULL);
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
