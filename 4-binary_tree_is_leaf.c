a87183f8971309adcf2ef60fecabdb655cf60847
rzsamrio <samuelekop144@gmail.com>
2024-03-01 03:47:49 +0100

    add task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_is_leaf - Insert node at right of parent
+ * @node: Node element
+ * Return: Node address
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left || node->right)
+		return (0);
+	return (1);
+}
